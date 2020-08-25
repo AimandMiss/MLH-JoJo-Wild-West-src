1d51b425c1c41f76434ea1a61337b1dee7d3f759
AimandMiss <42977104+AimandMiss@users.noreply.github.com>
2020-08-25 13:22:50 +0400

    Initial commit

---
+++ Assets/Assets/Scripts/DustEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DustEffect : MonoBehaviour
+{
+    public GameObject hitg;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(hitg, transform.position, Quaternion.identity);
+        if (collision.collider.CompareTag("Respawn"))
+        {
+            StickmanController.health = 0;
+        }
+        if (collision.collider.CompareTag("Finish") && StickmanController.health > 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
